39befcd1b895a254ab8dd363c039995b91a3099a
Jason Hao <jhao4@dons.usfca.edu>
2021-06-18 10:49:01 -0700

    Hello

--- timer-app/src/App.js
+++ timer-app/src/App.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import Background from './components/Background';
 import SettingsModal from './components/SettingsModal';
+import Time from './components/Time';
 
 function App() {
   const [settings, setSettings] = useState(true)
-  const [background, setBackground] = useState('does not exist')
+  const [background, setBackground] = useState('default')
 
   const settingsHandler = () => {
     setSettings(null);
+  }
+
+  const addSettings = (settingsBackground) => {
+    setBackground(settingsBackground);
   }
 
   return (
     <div>
       <Background image={background}>
         {settings &&
-          (<SettingsModal onConfirm={settingsHandler} />)
+          (<SettingsModal onAddSettings={addSettings} onConfirm={settingsHandler} />)
         }
+        <Time time={50} />
       </Background>
     </div>
   );
